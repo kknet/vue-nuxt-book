acc48817c6102fa6d5652dc4067109b251580fe4
yzbgyq <710851698@qq.com>
2019-07-03 15:06:07 +0800

    增加github登录1

--- server/js/auth.js
+++ server/js/auth.js
@@ -13,12 +13,14 @@ module.exports = app => {
             return
         }
         const env = process.env.NODE_ENV === "production"
+        console.log(env);
+        
         const data = await axios({
             method: 'POST',
             url: 'https://github.com/login/oauth/access_token',
             data: {
-                client_id: env ? 'd206e28818669ecf8e6c' : 'cc6571de853ab8c8f717',
-                client_secret: env ? '5c0ac02771adc39bc83c0b04dedc9546547e77b6' : '954a51ad022cea5f2db05823390893adf65083da',
+                client_id: env ? '26bc05886ab7acf7753a' : 'cc6571de853ab8c8f717',
+                client_secret: env ? '362a7336c5181fd58a4923a8d70706f39d68a196' : '954a51ad022cea5f2db05823390893adf65083da',
                 code,
             },
             headers: {
